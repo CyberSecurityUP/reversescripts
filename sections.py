83dfda7d9fe394b2d9ca0104d98ba1cc4099f26a
Cr4zyC0d3 <34966120+CyberSecurityUP@users.noreply.github.com>
2019-03-21 16:00:18 -0300

    Update sections.py

--- sections.py
+++ sections.py
@@ -3,7 +3,7 @@ from elftools.elf.elffile import ELFFile
 #coding: Non-ASCII
 print("WELCOME TO TOOL SECTIONS BY JOAS ANTONIO")
 
-with open('./chall.elf', 'rb') as f:
+with open('./chall.elf', 'rb') as f: #change the file name to what you are performing reverse engineering
     e = ELFFile(f)
     for section in e.iter_sections():
         print(hex(section[’sh_addr’]), section.name)
